84cbf1cddb9800c6f8057d2b0a048539565f397b
Vitor Nunes <stecks10@gmail.com>
2021-04-27 19:37:32 -0300

    Estilizando Listagem

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -3,7 +3,19 @@ import { Keyboard } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default class Main extends Component {
   state = {
@@ -17,7 +29,7 @@ export default class Main extends Component {
     const response = await api.get(`/users/${newUser}`);
 
     const data = {
-      name: PanResponder.data.name,
+      name: response.data.name,
       login: response.data.login,
       bio: response.data.bio,
       avatar: response.data.avatar_url,
@@ -44,12 +56,29 @@ export default class Main extends Component {
             value={newUser}
             onChangeText={text => this.setState({ newUser: text })}
             returnKeyType="send"
-            onSubmitEdting={this.handleAddUser}
+            onSubmitEditing={this.handleAddUser}
           />
           <SubmitButton onPress={this.handleAddUser}>
             <Icon name="add" size={20} color="#FFF"  />
           </SubmitButton>
         </Form>
+
+          <List
+            data={users}
+            keyExtractor={user => user.login}
+            renderItem={({ item }) => (
+              <User>
+                <Avatar source={{ uri: item.avatar }} />
+                <Name>{item.name}</Name>
+                <Bio>{item.bio}</Bio>
+
+                <ProfileButton onPress={() => {}}>
+                  <ProfileButtonText>Ver perfil</ProfileButtonText>
+                </ProfileButton>
+              </User>
+            )}
+          />
+
       </Container>
     );
   }

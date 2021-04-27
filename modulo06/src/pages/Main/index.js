aca1173d5e53750f5e1a1dff4262f2b0a140f0c2
Vitor Nunes <stecks10@gmail.com>
2021-04-27 16:55:22 -0300

    React Navigation

---
+++ modulo06/src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Main() {
+  return <View />;
+
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
